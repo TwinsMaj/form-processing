bbc620bed6bcee7a5837de611fddc6b3859c2fe6
Maja Taiwo <=>
2020-08-26 21:59:23 +0300

    bootstrap registration functionality

--- register.php
+++ register.php
@@ -1,3 +1,11 @@
+<?php # handle registration form
+
+    #be sure user clicked submit button
+    if(array_key_exists('register', $_POST)) {
+        var_dump($_POST);
+    }
+?>
+
 <!DOCTYPE html>
 <html>
     <head>
@@ -8,26 +16,28 @@
         <h1>Register</h1>
         <hr />
 
-        <form>
+        <form method="POST" action="">
             <div>
                 <label>First Name</label><br />
-                <input type="text" />
+                <input type="text" name="fn"/>
             </div>
 
             <div>
                 <label>Last Name</label><br />
-                <input type="text" />
+                <input type="text" name="ln"/>
             </div>
 
             <div>
                 <label>Email</label><br />
-                <input type="email" />
+                <input type="email" name="e"/>
             </div>
 
             <div>
                 <label>Password</label><br />
-                <input type="password" />
+                <input type="password" name="pass" />
             </div>
+
+            <div><input type="submit" name="register" value="register"/></div>
         </form>
     </body>
 </html>
